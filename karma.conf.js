250ad83c9d0f1fa9b7ba4e53611324404f0c2bf8
DavidGWheeler <wots_uh_thedeal@msn.com>
2017-06-14 21:42:15 -0700

    add phantomjs dependency and change karma.conf logLevel to INFO

--- karma.conf.js
+++ karma.conf.js
@@ -22,7 +22,7 @@ module.exports = function(config) {
     reporters: ['mocha'],
     port: 9876,
     colors: true,
-    logLevel: config.LOG_DEBUG,
+    logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['PhantomJS'],
     singleRun: false,
